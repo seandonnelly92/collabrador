bd5b96578ea0eb9dc0c356c91db4763cea3608d3
Thomas Ellwood <taellwood@gmail.com>
2024-02-27 15:15:38 +0000

    changed column 'type' on appointments to 'looking_for'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_114946) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_145917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_114946) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.string "location"
-    t.string "type"
+    t.string "looking_for"
     t.bigint "pet_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
