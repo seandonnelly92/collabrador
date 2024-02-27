a386e85c8ef0046bf6849b98373ae4244ec37c77
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-27 18:19:04 +0000

    Added basic signup modal. Based on rendered registration form in modal. Resources based on Applications Controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller? # Added this to call the configure_permitted_parameters method
+  before_action :create_signup_resources
+
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :first_name, :last_name, :postcode])
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :first_name, :last_name, :postcode])
+  end
+
+  def create_signup_resources
+    @resource = User.new
+    @resource_name = :user
   end
 end
