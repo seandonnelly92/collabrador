33827a108302766913ecb31c1bf781f78cefda1b
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-28 08:32:18 +0000

    Included modal for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller? # Added this to call the configure_permitted_parameters method
-  before_action :create_signup_resources
+  before_action :create_user_signup_resources, :create_user_login_resources, :devise_mapping
 
   private
 
@@ -9,8 +9,17 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :first_name, :last_name, :postcode])
   end
 
-  def create_signup_resources
-    @resource = User.new
-    @resource_name = :user
+  def create_user_signup_resources
+    @signup_resource = User.new
+    @signup_resource_name = :user
+  end
+
+  def create_user_login_resources
+    @login_resource = User.new
+    @login_resource_name = :user
+  end
+
+  def devise_mapping
+    @devise_mapping = Devise.mappings[:user] # Used for login
   end
 end
--- app/javascript/controllers/modals_controller.js
+++ app/javascript/controllers/modals_controller.js
@@ -1,20 +1,40 @@
 import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from "stimulus-use"
 
 // Connects to data-controller="modals"
 export default class extends Controller {
   static targets = [
-    "signup"
+    "signup",
+    "signin",
+    "closeBtn"
   ]
 
   connect() {
     console.log("Hello from the modals controller");
+    useClickOutside(this, { element: this.signupTarget });
   }
 
   openSignup() {
     this.signupTarget.classList.add('show');
   }
 
-  closeSignup() {
+  clickOutside(e) {
+    e.preventDefault();
+    // this.signupTarget.classList.remove('show');
+    // this.signupTarget.close()
+  }
+
+  closeSignup(e) {
     this.signupTarget.classList.remove('show');
+  }
+
+  // Methods for login
+  openSignin(e) {
+    console.log(e.currentTarget);
+    this.signinTarget.classList.add('show');
+  }
+
+  closeSignin(e) {
+    this.signinTarget.classList.remove('show');
   }
 }
