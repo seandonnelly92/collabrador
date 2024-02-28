b35441d235ef429675627754317c934668233b67
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-28 12:07:23 +0000

    Removed temp action for the Pets controller. Also removed from routes.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,7 +2,4 @@ class PetsController < ApplicationController
   def index
     @pets = Pet.all
   end
-
-  def temp
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  get "temp", to: "pets#temp"
 end
