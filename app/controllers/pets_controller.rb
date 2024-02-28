f2a22281897cb8420529510fa4c2afccf550609d
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-28 14:41:08 +0000

    Completed new pet modal. Updated pets controller for create action which redirects back to the user_profile page

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,4 +7,20 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     @owner = @pet.user
   end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to user_profile_path
+    else
+      render user_profile_path, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :animal_type, :breed, :size, :activity_needs, :neutered_spayed, :child_friendly, :dog_friendly, :cat_friendly, :hypoallergenic)
+  end
 end
