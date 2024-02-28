de956e79a27e71c40e4c2d11707d64cd8fbc10e5
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-28 15:20:14 +0000

    Updated sign-up to redirect to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "registrations/registrations"
+  }
 
   root to: "pets#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
