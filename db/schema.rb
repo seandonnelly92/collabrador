c62ff527bc8c47314a0cb0cc503cfd557adf2d31
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-02-29 20:47:14 +0000

    Created migration for pending_helper column in Appointments, to confirm appointments between owner and helper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_161753) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_204627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_161753) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "pending_migration"
+    t.bigint "pending_helper"
     t.index ["pet_id"], name: "index_appointments_on_pet_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
