997ff0150203d4998c44381a4c258ab8536e02dc
Thomas Ellwood <taellwood@gmail.com>
2024-02-29 13:39:02 +0000

    map showing markers with image and name

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -21,6 +21,7 @@ export default class extends Controller {
     this.#fitMapToMarkers()
   }
 
+
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.map_marker_html)
@@ -36,4 +37,6 @@ export default class extends Controller {
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
+
+
 }
