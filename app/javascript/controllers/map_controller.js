4fc5da085c0895572614ed2c6796e288da05a779
Thomas Ellwood <taellwood@gmail.com>
2024-02-29 11:56:28 +0000

    added map to home (index) page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,7 +1,20 @@
-import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="map"
+// app/javascript/controllers/map_controller.js
+import { Controller } from "@hotwired/stimulus"
+import mapboxgl from 'mapbox-gl' // Don't forget this!
+
 export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
   connect() {
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
   }
 }
