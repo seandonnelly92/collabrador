a90523a0a6c747402ac25cb27a3c07a8e3a4aa83
Thomas Ellwood <taellwood@gmail.com>
2024-03-01 12:54:57 +0000

    changed stlying on marker

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,3 +1,91 @@
+// import { Controller } from "@hotwired/stimulus"
+// import mapboxgl from 'mapbox-gl' // Don't forget this!
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+// export default class extends Controller {
+//   static values = {
+//     apiKey: String,
+//     postcode: String
+//   }
+
+//   static targets = ["map"];
+
+//   connect() {
+//     mapboxgl.accessToken = this.apiKeyValue
+
+//     this.map = new mapboxgl.Map({
+//       container: this.element,
+//       style: "mapbox://styles/mapbox/streets-v10",
+//       center: [-0.1372148, 51.5048867],
+//       zoom: 12,
+//       speed: 1.5
+//     })
+
+//     console.log("I am connected")
+
+//     const purpose = this.data.get("mapPurpose");
+
+//     if (purpose === "general") {
+//       this.setupGeneralMap();
+//     } else if (purpose === "specific") {
+//       this.setupSpecificMap();
+//     }
+
+//     this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }))
+//   }
+
+//   setupGeneralMap() {
+//     // General map setup code
+//     this.addMarkersToMap(); // Add markers to the map
+//     this.fitMapToMarkers(); // Fit map bounds to markers
+//   }
+
+//   setupSpecificMap() {
+//     // Specific map setup code
+//     const postcode = this.data.get("postcode");
+//     // You can use the postcode to set up your map accordingly
+//     // Example: You can use Geocoding API to get coordinates for the given postcode and then set the map center accordingly
+//     // For demonstration, let's assume the coordinates are hardcoded here
+//     const coordinates = [-0.1372148, 51.5048867]; // Example coordinates
+//     this.map.setCenter(coordinates);
+//     // You can also add a marker for the specific location
+//     new mapboxgl.Marker()
+//       .setLngLat(coordinates)
+//       .addTo(this.map);
+//   }
+
+//   addMarkersToMap() {
+//     // Retrieve markers data
+//     const markers = JSON.parse(this.element.getAttribute("data-map-markers-value"));
+
+//     // Add markers to the map
+//     markers.forEach((marker) => {
+//       const popup = new mapboxgl.Popup().setHTML(marker.map_marker_html);
+//       new mapboxgl.Marker()
+//         .setLngLat([marker.lng, marker.lat])
+//         .setPopup(popup)
+//         .addTo(this.map);
+//     });
+//   }
+
+//   fitMapToMarkers() {
+//     // Fit map bounds to markers
+//     const bounds = new mapboxgl.LngLatBounds();
+//     const markers = JSON.parse(this.element.getAttribute("data-map-markers-value"));
+//     markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+//     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+//   }
+// }
+
+
+
+
+
+
+
+
+//NEW CODE BELOW!
+
 
 // app/javascript/controllers/map_controller.js
 import { Controller } from "@hotwired/stimulus"
@@ -24,6 +112,8 @@ export default class extends Controller {
     })
 
     console.log("I am connected")
+
+
 
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
