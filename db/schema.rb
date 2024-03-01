18376da73986d693c06a6fa9dc71a9089bf82d0a
Jasper Warmenhoven <jasper.warmenhoven@hotmail.com>
2024-03-01 14:23:59 +0000

    Updated profile page picture syntax

--- db/schema.rb
+++ db/schema.rb
@@ -100,9 +100,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_01_130304) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture_url"
     t.float "latitude"
     t.float "longitude"
+    t.string "picture_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
